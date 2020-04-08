75a62bd4048e591f858d33dc9b02978981c8d59d
GeonwooVincentKim <kdsnop@gmail.com>
2020-04-08 21:51:07 +0900

    Please God..

--- mychat/wsgi.py
+++ mychat/wsgi.py
@@ -11,8 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+# import django_heroku
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychat.settings')
 
+# django_heroku.settings(locals())
 application = get_wsgi_application()
 application = DjangoWhiteNoise()
